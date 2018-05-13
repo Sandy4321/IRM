1d23fee980da793e35a1f5331e4246a69f7c63f5
Maasamiya <masa.mm2@gmail.com>
2018-05-13 16:13:56 +0900

    CRPのClass(複数バージョン)

---
+++ Main/CRP/main_CRP.cpp
@@ -0,0 +1,17 @@
+// CRP.cpp
+// CRPの過程に従って自動的に客を分割
+// 入力:CRPのパラメータalpha,CRPの試行回数(客の人数)
+// 出力:各客の座り方, 各机の客の人数
+#include <iostream>
+#include <random>
+#include <vector>
+#include "CRP.hpp"
+
+int main() {
+  CRP C;
+  C.get_CRP_parameter();
+  C.CRP_prrocedure();
+  // C.show_CRP_parameter();
+  C.show_customer_datas();
+  return 0;
+}
