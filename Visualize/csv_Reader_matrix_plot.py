ca53bac4e7a6fb0646a286022440f08282137a8d
Maasamiya <masa.mm2@gmail.com>
2018-05-21 16:52:38 +0900

    データのビジュアル化

--- Visualize/csv_Reader_matrix_plot.py
+++ Visualize/csv_Reader_matrix_plot.py
@@ -8,11 +8,11 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import csv
 csv_int=[list(map(int,line.rstrip().split(","))) for line in open('Output_Binary_Relation_Matrix.csv').readlines()]
-csv_label=[list(map(int,line.rstrip().split(","))) for line in open('Panda_out.csv').readlines()]
+csv_label=[list(map(float,line.rstrip().split(","))) for line in open('Output_size_Parameter_Relation_Matrix.csv').readlines()]
 csv_label_list=np.array(csv_label)
 
-tate =[list(map(int,line.rstrip().split(","))) for line in open('tatehidden_K.csv').readlines()] 
-yoko=[list(map(int,line.rstrip().split(","))) for line in open('yokohidden_L.csv').readlines()]
+tate =[list(map(int,line.rstrip().split(","))) for line in open('hidden_K.csv').readlines()] 
+yoko=[list(map(int,line.rstrip().split(","))) for line in open('hidden_L.csv').readlines()]
 
 
 
@@ -30,9 +30,11 @@ print Output_sorted_by_both
 Output_Parameter_label = pd.DataFrame(data=csv_label_list, index=tate,columns=x)
 Output_Parameter_label_sorted_by_index = Output_Parameter_label.sort_index()
 Output_Parameter_label_sorted_by_both = Output_Parameter_label_sorted_by_index.sort_index(axis=1)
+print Output_Parameter_label_sorted_by_both
+
 
 plt.figure()
-sns.heatmap(Output_sorted_by_both, square=True,annot=Output_Parameter_label_sorted_by_both,cmap='Blues')
+sns.heatmap(Output_Parameter_label_sorted_by_both, square=True,annot=Output_sorted_by_both,fmt='.1g',cmap='Blues')
 plt.savefig('seaborn_heatmap_list.png')
 plt.show()
 plt.close('all')
