f52ddef91c3dcef6f0614c93f9d153e997868dd7
Maasamiya <masa.mm2@gmail.com>
2018-05-27 10:39:12 +0900

    入力alphaのバグがないことを確認

--- Main/CRP/main_CRP.cpp
+++ Main/CRP/main_CRP.cpp
@@ -14,20 +14,13 @@ int main(int argc, const char *argv[]) {
   }
 
   int customer_number = atoi(argv[1]);  //コマンドラインからに試行回数を入力
-                                        // C.set_CRP_customer_number_alpha_1(customer_number);
-  // C.get_CRP_parameter();//手動入力
-  // C.run_CRP();
-  // C.show_CRP_parameter();
-  // C.show_customer_datas();
+                                        //
 
   double average_desk = 0;
-  int Repeat_number = 1000;//暫定で決定
+  int Repeat_number = 1000;  //暫定で決定
   for (auto j = 0; j < Repeat_number; j++) {
     CRP C;
-    C.set_CRP_customer_number_alpha_1(customer_number);
-    // C.get_CRP_parameter();//手動入力
-    // C.run_CRP();
-
+    C.set_CRP_customer_number_alpha_1(customer_number); 
     C.run_CRP();
     C.show_customer_datas();
     average_desk += (double)C.get_desk_number();
@@ -36,5 +29,13 @@ int main(int argc, const char *argv[]) {
 
   // average_desk=C.CRP_repeat();
   std::cout << "average_desk=" << average_desk << std::endl;
+
+  /*
+    CRP C;
+    C.get_CRP_parameter();  //手動入力
+    C.run_CRP();
+    C.show_CRP_parameter();
+    C.show_customer_datas();
+  */
   return 0;
 }
